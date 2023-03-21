4a9297f2db4217b9b16f7b1767222f98212ecc3a
Shu Tanabe <sfcofc@gmail.com>
2023-03-21 12:43:23 +0900

    NextAuth 素振り Google認証

---
+++ src/pages/api/movies/list.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]";
+
+export default async function listMovies(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getServerSession(req, res, authOptions);
+  if (session) {
+    res.send({
+      movies: [
+        { title: "Alien vs Predator", id: 1 },
+        { title: "Reservoir Dogs", id: 2 },
+      ],
+    });
+  } else {
+    res.send({
+      error: "You must sign in to view movies.",
+    });
+  }
+}
